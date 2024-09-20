e354757a377373c40dcfab99d9686bb0d86f1ace
Brian Vanderwende <vanderwb@ucar.edu>
2024-09-20 13:46:02 -0600

    Potential fix for missing NCAR_HOST

--- util/mod_init.sh
+++ util/mod_init.sh
@@ -1,11 +1,22 @@
-# First source the modules init
-. /glade/u/apps/$NCAR_HOST/default/util/localinit.sh
+# Let's ensure we cannot repeat this...
+if [[ -z $__PBS_MOD_INIT_HACK ]]; then
+    export __PBS_MOD_INIT_HACK=1
 
-# Now remove this hack
-unset BASH_ENV
+    # First source the modules init
+    if [[ -n $NCAR_HOST ]]; then
+        . /glade/u/apps/$NCAR_HOST/default/util/localinit.sh
+    elif [[ $(hostname) == de* ]]; then
+        . /glade/u/apps/derecho/default/util/localinit.sh
+    else
+        . /glade/u/apps/casper/default/util/localinit.sh
+    fi
 
-# Update modules to clean up path redundencies
-# Can't actually use "module update" as that will change versions on you (to defaults)
-PBS_O_MODULES=$(module -t list |& xargs)
-module -q purge
-module -q load $PBS_O_MODULES
+    # Now remove this hack
+    unset BASH_ENV
+
+    # Update modules to clean up path redundencies
+    # Can't actually use "module update" as that will change versions on you (to defaults)
+    PBS_O_MODULES=$(module -t list |& xargs)
+    module -q purge
+    module -q load $PBS_O_MODULES
+fi
